e2e9ac82419e8225aab06a7ab71ac18d7346fde6
Admin <mutobui@gmail.com>
2021-04-19 15:28:17 +0700

    get_block

--- src/controllers/block_controller.js
+++ src/controllers/block_controller.js
@@ -1,7 +1,34 @@
-// Defining all methods and business logic for routes
+const { EventService, CasperServiceByJsonRPC } = require('casper-client-sdk');
+const { exec } = require("child_process");
+
+require('dotenv').config();
 
 module.exports = {
   GetLatestBlock: function (req, res) {
-    res.json("Get the latest block!");
+    // console.log(process.env.NETWORK_RPC_API);
+
+    // let service = new CasperServiceByJsonRPC(process.env.NETWORK_RPC_API);
+
+    // service.getLatestBlockInfo().then(value => {
+    //   console.log("Value: ", value);
+    // }).catch(err => {
+    //   console.log("Err: ", err);
+    // })
+
+    exec(`./casper-client get-block --node-address ${process.env.NETWORK_RPC_API}`, (error, stdout, stderr) => {
+      if (error) {
+        res.status(500);
+        res.json(error);
+        return;
+      }
+      if (stderr) {
+        res.status(500);
+        res.json(stderr);
+        return;
+
+      }
+      res.status(200);
+      res.json(JSON.parse(stdout));
+    });
   }
 };
