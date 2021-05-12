be333c74418f266c2a3a387d7677e5adff5a4ecc
Admin <mutobui@gmail.com>
2021-05-12 11:59:31 +0700

    add core middleware

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+var corsOptions = {
+    origin: 'http://casperstats.io',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+/* Nhiều domain */
+var whitelist = ['http://casperstats.com']
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled ' })
+})
+
 app.use(express.json());
 app.use(routes);
 
