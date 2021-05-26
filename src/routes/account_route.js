6f405d7a5584b856ab55a69ab066b30dac889b25
Admin <mutobui@gmail.com>
2021-05-26 10:36:53 +0700

    fix get-account

--- src/routes/account_route.js
+++ src/routes/account_route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const account_controller = require('../controllers/account_controller');
 
-router.route("/get-account/:address").get(account_controller.GetAccount);
+router.route("/get-account/:account").get(account_controller.GetAccount);
 // router.route("/get-holder/:account").get(account_controller.GetHolder);
 router.route("/get-transfers").get(account_controller.GetAccountTransfers);
 router.route("/get-deploys/").get(account_controller.GetAccountDeploys);
