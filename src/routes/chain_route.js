374df5606ac6513c4917012cb25ad0f516dc7c0e
Admin <mutobui@gmail.com>
2021-07-05 15:58:05 +0700

    cache for get-block

--- src/routes/chain_route.js
+++ src/routes/chain_route.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const chain_controller = require('../controllers/chain_controller');
 
-router.route("/get-block/:block").get(chain_controller.GetBlock);
+const verifyBlock = (req, res, next) => {
+    try {
+        const block = req.params.block
+        if (chain_controller.cache.has(block)) {
+            return res.status(200).json(chain_controller.cache.get(block));
+        }
+        return next();
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+router.route("/get-block/:block").get(verifyBlock, chain_controller.GetBlock);
+
 router.route("/get-latest-blocks/:number").get(chain_controller.GetLatestBlocks);
 router.route("/get-block-transfers/:block").get(chain_controller.GetBlockTx);
 router.route("/get-state-root-hash/:block").get(chain_controller.GetStateRootHash);
