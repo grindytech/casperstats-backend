c18689cf0b497732dacd1a79c627e5affc0f1f27
Hoang Trung <lhtrung307@gmail.com>
2022-09-28 21:19:33 +0700

    Config eslint for project. Upgrade casper sdk.

--- src/app.js
+++ src/app.js
@@ -1,40 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-const bodyParser = require('body-parser');
-const cron = require('./cron-job');
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cron = require("./cron-job");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3307;
 const HOST = process.env.SERVER_HOST || "127.0.0.1";
 // Specific domain
-const allowedOrigins =  JSON.parse(process.env.ALLOW_ORIGINS);
+const allowedOrigins = JSON.parse(process.env.ALLOW_ORIGINS);
 
-require('./models');
+require("./models");
 
 app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 /* CROS middleware */
 app.use(function (req, res, next) {
-
   // All domain
   // res.header("Access-Control-Allow-Origin", "*");
 
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // icon for bookmarks
-app.get('/favicon.ico', (req, res) => {
+app.get("/favicon.ico", (req, res) => {
   res.sendStatus(200);
 });
 
@@ -43,6 +45,6 @@ app.use(routes);
 
 cron.start();
 
-app.listen(PORT, HOST,  () => {
+app.listen(PORT, HOST, () => {
   console.log(`Server is running on port: ${PORT} ${HOST}`);
-})
+});
