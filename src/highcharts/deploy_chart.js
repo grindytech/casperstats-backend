34fab66fdab11f5a1729c70888ab31cb4564ab0d
Bao-CryptoViet <baonguyen120301@gmail.com>
2022-10-14 16:32:41 +0700

    change color and type of some charts

--- src/highcharts/deploy_chart.js
+++ src/highcharts/deploy_chart.js
@@ -18,8 +18,25 @@ async function deployChart() {
       text: "",
     },
     colors: ["#B6B6B4"],
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1.3,
+          },
+          stops: [
+            [0, "rgba(0, 0, 0, 0.2)"],
+            [1, "rgba(255,255,255,0)"],
+          ],
+        },
+      },
+    },
     series: [
       {
+        type: "area",
         data: deploy_tx.reverse(),
       },
       {
--- src/highcharts/marketCap_chart.js
+++ src/highcharts/marketCap_chart.js
@@ -10,6 +10,7 @@ async function marketCapChart() {
     },
     series: [
       {
+        type: "area",
         data: data.reverse(),
       },
     ],
@@ -17,6 +18,22 @@ async function marketCapChart() {
       enabled: false,
     },
     colors: ["#B6B6B4"],
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1.3,
+          },
+          stops: [
+            [0, "rgba(0, 0, 0, 0.2)"],
+            [1, "rgba(255,255,255,0)"],
+          ],
+        },
+      },
+    },
     xAxis: {
       type: "datetime",
     },
