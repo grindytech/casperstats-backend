d59ffaf5f390eb6abec9c865a85dcb084c3c29f4
bao_cryptoviet <baonguyen120301@gmail.com>
2022-11-10 16:18:25 +0700

    add validato for input in chain_route

--- src/middleware/schemas.js
+++ src/middleware/schemas.js
@@ -4,8 +4,11 @@ const Joi = require("joi");
 // Casper networks are compatible with both Ed25519 and secp256k1 public key cryptography.
 const account = Joi.string().alphanum().min(64).max(68).required();
 
-const start = Joi.number().min(0);
-const count = Joi.number().min(1);
+const start = Joi.number().min(0).required();
+const count_or_end = Joi.number().min(1).required();
+
+const block = Joi.number().min(0).required();
+const number = Joi.number().min(1).max(30).required();
 
 const schemas = {
   account: Joi.object().keys({
@@ -13,12 +16,27 @@ const schemas = {
   }),
   startToCount: Joi.object().keys({
     start: start,
-    count: count,
+    count: count_or_end,
   }),
   startToCountWithAccount: Joi.object().keys({
     account: account,
     start: start,
-    count: count,
+    count: count_or_end,
+  }),
+  startToCountWithValidator: Joi.object().keys({
+    validator: account,
+    start: start,
+    count: count_or_end,
+  }),
+  block: Joi.object().keys({
+    block: block,
+  }),
+  number: Joi.object().keys({
+    number: number,
+  }),
+  startToEnd: Joi.object().keys({
+    start: start,
+    end: count_or_end,
   }),
 };
 
