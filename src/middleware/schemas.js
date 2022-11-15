96c348793265e4f98b47916786e3e59758805d4d
bao_cryptoviet <baonguyen120301@gmail.com>
2022-11-15 10:35:59 +0700

    add validate input for info_route

--- src/middleware/schemas.js
+++ src/middleware/schemas.js
@@ -10,6 +10,28 @@ const count_or_end = Joi.number().min(1).required();
 const block = Joi.number().min(0).required();
 const number = Joi.number().min(1).max(30).required();
 
+const hex = Joi.string().alphanum().length(64).required();
+
+// type to get blockchain data
+const blockchain_data_type = Joi.string().valid(
+  "transfer",
+  "transfer_tx",
+  "deploy_tx",
+  "deploy",
+  "delegator",
+  "validator",
+  "bid",
+  "active_bid",
+  "price",
+  "market_cap",
+  "total_volume",
+  "staking",
+  "staking_tx",
+  "unstaking",
+  "unstaking_tx",
+  "total_supply"
+);
+
 const schemas = {
   account: Joi.object().keys({
     account: account,
@@ -37,6 +59,12 @@ const schemas = {
   startToEnd: Joi.object().keys({
     start: start,
     end: count_or_end,
+  }),
+  hex: Joi.object().keys({
+    hex: hex,
+  }),
+  blockchain_data_type: Joi.object().keys({
+    type: blockchain_data_type,
   }),
 };
 
